5a5b4dc68399cb8ce6fb88bb66bd21dd7707f0db
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2025-04-17 17:10:51 +0400

    folder

--- src/zophar/models.py
+++ src/zophar/models.py
@@ -1,12 +1,19 @@
+from __future__ import annotations
+
 import dataclasses as dc
 import datetime as dt
 from typing import Literal, Mapping
 
+from slugify import slugify
 from yarl import URL
 
 type PageType = Literal["infopage", "gamelist", "gamepage"]
-type MenuItems = Mapping[str, str]
+
+type MenuItems = Mapping[str, Folder]
+"""Menu items: path - name"""
+
 type Menu = Mapping[str, MenuItems]
+"""Menu: `Root name` - `Menu item`"""
 
 type Platforms = Mapping[str, str]
 
@@ -21,6 +28,50 @@ class GameTrack:
     """Duration"""
     url: Mapping[str, URL]
     """Mapping with URLs to audio files by it's extension"""
+
+
+@dc.dataclass(slots=True)
+class Folder:
+    """Browsable entity. Have `path` property."""
+
+    path: str
+    """URL relative path"""
+    name: str
+    """Name"""
+    children: dict[str, Folder] = dc.field(default_factory=dict)
+    """Children"""
+
+    def resolve(self, path: str) -> Folder:
+        """"""
+
+        if not path:
+            return self
+
+        id, _, path = path.partition("/")
+
+        if x := self.children.get(id):
+            return x.resolve(path)
+
+        raise KeyError("")
+
+    def add(self, path: str, item: str | Folder) -> str:
+        """"""
+
+        parent, _, id = path.rpartition("/")
+
+        if isinstance(item, str):
+            name = item
+            item = Folder(path, item)
+
+        else:
+            name = item.name
+
+        if not id:
+            id = slugify(name)
+
+        self.resolve(parent).children[id] = item
+
+        return id
 
 
 @dc.dataclass(slots=True)
